81fc1960d08b583bc86f5bef05ab42250db90c36
shaloo <5890484+shaloo@users.noreply.github.com>
2023-03-31 20:33:21 +0530

    Improve JS logic

--- docs/javascripts/andropdown.js
+++ docs/javascripts/andropdown.js
@@ -1,91 +1,58 @@
-/*
-function myfun() {
-    var x = document.getElementById('state').selectedIndex;
-    var text = document.getElementsByTagName("option")[x].text;//its a text
-    var newtext = "<a class=\"md-button\" href=\"https://arcana.network\">"+text+"</a>";
-    document.getElementById("pr").innerHTML = newtext;
-  }
-  myfun();
-  */
-
 const selection = document.getElementById("selection");
-const appType1 = document.getElementById("appType1");
-const appType2 = document.getElementById("appType2");
-const appType3 = document.getElementById("appType3");
-const appType4 = document.getElementById("appType4");
-const appType5 = document.getElementById("appType5");
+const appTypes = [
+  document.getElementById("appType1"),
+  document.getElementById("appType2"),
+  document.getElementById("appType3"),
+  document.getElementById("appType4"),
+  document.getElementById("appType5")
+];
 const sidebarText = document.getElementById("sec-sidebar-text");
 
-function fetchPageContent(pageUrl,appType) {
-    fetch(pageUrl)
-        .then(response => response.text())
-        .then(text => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(text, 'text/html');
-            const mainContent = doc.querySelector('div.md-content').innerHTML;
-            const sidebarSecondary = doc.querySelector('div.md-sidebar--secondary').innerHTML;
-            document.getElementById(appType).innerHTML = mainContent;
-            document.getElementById('sec-sidebar-text').innerHTML = sidebarSecondary;
-        })
-        .catch(error => {
-            console.error(error);
-        });
+function fetchPageContent(pageUrl) {
+  return fetch(pageUrl)
+    .then(response => response.text())
+    .then(text => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(text, 'text/html');
+      const mainContent = doc.querySelector('div.md-content').innerHTML;
+      const sidebarSecondary = doc.querySelector('div.md-sidebar--secondary').innerHTML;
+      return { mainContent, sidebarSecondary };
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
-selection.addEventListener("change", function() {
-	if (selection.value === "option1") {
-        fetchPageContent('./howto/integrate_auth/integrate_wallet.html',"appType1");
-		appType1.style.display = "block";
-		appType2.style.display = "none";
-        appType3.style.display = "none";
-        appType4.style.display = "none";
-        appType5.style.display = "none";
-        sidebarText.style.display = "block";
-	} else if (selection.value === "option2") {
-        fetchPageContent('./howto/integrate_auth/integrate_wallet_react.html',"appType2");
-		appType1.style.display = "none";
-		appType2.style.display = "block";
-		appType3.style.display = "none";
-        appType4.style.display = "none";
-        appType5.style.display = "none";
-        sidebarText.style.display = "block";
-	} else if (selection.value === "option3") {
-        fetchPageContent('./howto/integrate_auth/integrate_wagmi.html',"appType3");
-        appType1.style.display = "none";
-        appType2.style.display = "none";
-        appType3.style.display = "block";
-        appType4.style.display = "none";
-        appType5.style.display = "none";
-        sidebarText.style.display = "block";
-    } else if (selection.value === "option4") {
-        fetchPageContent('./howto/integrate_auth/integrate_rainbow.html',"appType4");
-        appType1.style.display = "none";
-        appType2.style.display = "none";
-        appType3.style.display = "none";
-        appType4.style.display = "block";
-        appType5.style.display = "none";
-        sidebarText.style.display = "block";
-    } else if (selection.value === "option5") {
-        fetchPageContent('./howto/integrate_auth/integrate_wallet_vue.html',"appType5");
-        appType1.style.display = "none";
-        appType2.style.display = "none";
-        appType3.style.display = "none";
-        appType4.style.display = "none";
-        appType5.style.display = "block";
-        sidebarText.style.display = "block";
-    }
-      else {
-        appType1.style.display = "none";
-		appType2.style.display = "none";
-		appType3.style.display = "none";
-        appType4.style.display = "none";
-        appType5.style.display = "none";
-        sidebarText.style.display = "none";
-	}
+const pageData = {
+  option1: { url: './howto/integrate_auth/integrate_wallet.html', appType: 0 },
+  option2: { url: './howto/integrate_auth/integrate_wallet_react.html', appType: 1 },
+  option3: { url: './howto/integrate_auth/integrate_wagmi.html', appType: 2 },
+  option4: { url: './howto/integrate_auth/integrate_rainbow.html', appType: 3 },
+  option5: { url: './howto/integrate_auth/integrate_wallet_vue.html', appType: 4 }
+};
+
+selection.addEventListener("change", async function() {
+  const option = selection.value;
+  if (pageData.hasOwnProperty(option)) {
+    const { url, appType } = pageData[option];
+    const { mainContent, sidebarSecondary } = await fetchPageContent(url);
+    appTypes.forEach((app, index) => {
+      app.style.display = index === appType ? "block" : "none";
+    });
+    sidebarText.style.display = "block";
+    document.getElementById(appTypes[appType].id).innerHTML = mainContent;
+    sidebarText.innerHTML = sidebarSecondary;
+  } else {
+    appTypes.forEach(app => {
+      app.style.display = "none";
+    });
+    sidebarText.style.display = "none";
+  }
 });
 
 window.onload = function() {
-    fetchPageContent("./howto/integrate_auth/integrate_wallet.html","appType1");
-}
-
-
+  fetchPageContent("/howto/integrate_auth/integrate_wallet.html")
+    .then(({ mainContent }) => {
+      document.getElementById(appTypes[0].id).innerHTML = mainContent;
+    });
+};
--- docs/quick_start.md
+++ docs/quick_start.md
@@ -1,6 +1,8 @@
 ---
-template: quick_start.html
-title: Quick Start
+template:  quick_start.html
+title: Interactive Quick Start
 ---
+
+# New Quick Start
 
 This is the default Quick Start content.
