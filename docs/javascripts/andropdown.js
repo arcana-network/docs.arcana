7798d01a48a85c12417284d425f600b08f7445f1
shaloo <5890484+shaloo@users.noreply.github.com>
2023-03-31 18:27:03 +0530

    update 1

--- docs/javascripts/andropdown.js
+++ docs/javascripts/andropdown.js
@@ -9,13 +9,12 @@ function myfun() {
   */
 
 const selection = document.getElementById("selection");
-const textA = document.getElementById("textA");
-const textB = document.getElementById("textB");
-const textC = document.getElementById("textC");
-const textD = document.getElementById("textD");
+const appType1 = document.getElementById("appType1");
+const appType2 = document.getElementById("appType2");
+const appType3 = document.getElementById("appType3");
+const appType4 = document.getElementById("appType4");
+const appType5 = document.getElementById("appType5");
 const sidebarText = document.getElementById("sec-sidebar-text");
-
-const pageUrl = './troubleshooting.html';
 
 function fetchPageContent(pageUrl,appType) {
     fetch(pageUrl)
@@ -35,45 +34,58 @@ function fetchPageContent(pageUrl,appType) {
 
 selection.addEventListener("change", function() {
 	if (selection.value === "option1") {
-        fetchPageContent('./howto/integrate_auth/integrate_wallet.html',"textA");
-		textA.style.display = "block";
-		textB.style.display = "none";
-        textC.style.display = "none";
-        textD.style.display = "none";
+        fetchPageContent('./howto/integrate_auth/integrate_wallet.html',"appType1");
+		appType1.style.display = "block";
+		appType2.style.display = "none";
+        appType3.style.display = "none";
+        appType4.style.display = "none";
+        appType5.style.display = "none";
         sidebarText.style.display = "block";
 	} else if (selection.value === "option2") {
-        fetchPageContent('./howto/integrate_auth/integrate_wallet_react.html',"textB");
-		textA.style.display = "none";
-		textB.style.display = "block";
-		textC.style.display = "none";
-        textD.style.display = "none";
+        fetchPageContent('./howto/integrate_auth/integrate_wallet_react.html',"appType2");
+		appType1.style.display = "none";
+		appType2.style.display = "block";
+		appType3.style.display = "none";
+        appType4.style.display = "none";
+        appType5.style.display = "none";
         sidebarText.style.display = "block";
 	} else if (selection.value === "option3") {
-        fetchPageContent('./howto/integrate_auth/integrate_wagmi.html',"textC");
-        textA.style.display = "none";
-        textB.style.display = "none";
-        textC.style.display = "block";
-        textD.style.display = "none";
+        fetchPageContent('./howto/integrate_auth/integrate_wagmi.html',"appType3");
+        appType1.style.display = "none";
+        appType2.style.display = "none";
+        appType3.style.display = "block";
+        appType4.style.display = "none";
+        appType5.style.display = "none";
         sidebarText.style.display = "block";
     } else if (selection.value === "option4") {
-        fetchPageContent('./howto/integrate_auth/integrate_wallet_vue.html',"textD");
-        textA.style.display = "none";
-        textB.style.display = "none";
-        textC.style.display = "none";
-        textD.style.display = "block";
+        fetchPageContent('./howto/integrate_auth/integrate_rainbow.html',"appType4");
+        appType1.style.display = "none";
+        appType2.style.display = "none";
+        appType3.style.display = "none";
+        appType4.style.display = "block";
+        appType5.style.display = "none";
         sidebarText.style.display = "block";
-    } 
+    } else if (selection.value === "option5") {
+        fetchPageContent('./howto/integrate_auth/integrate_wallet_vue.html',"appType5");
+        appType1.style.display = "none";
+        appType2.style.display = "none";
+        appType3.style.display = "none";
+        appType4.style.display = "none";
+        appType5.style.display = "block";
+        sidebarText.style.display = "block";
+    }
       else {
-        textA.style.display = "none";
-		textB.style.display = "none";
-		textC.style.display = "none";
-        textD.style.display = "none";
-        sidebarText.style.display = "block";
+        appType1.style.display = "none";
+		appType2.style.display = "none";
+		appType3.style.display = "none";
+        appType4.style.display = "none";
+        appType5.style.display = "none";
+        sidebarText.style.display = "none";
 	}
 });
 
 window.onload = function() {
-    fetchPageContent("./howto/integrate_auth/integrate_wallet.html","textA");
+    fetchPageContent("./howto/integrate_auth/integrate_wallet.html","appType1");
 }
 
 
